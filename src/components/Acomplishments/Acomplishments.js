ae01745bf015f2927217d1dd1082138ead963f35
safinghoghabori <safinghoghabori65@gmail.com>
2021-10-24 11:06:39 +0530

    accomplishment section added

---
+++ src/components/Acomplishments/Acomplishments.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+import {
+  Section,
+  SectionDivider,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import { Box, Boxes, BoxNum, BoxText } from "./AcomplishmentsStyles";
+
+import { accomplishments } from "../../constants/constants";
+
+const Acomplishments = () => (
+  <Section>
+    <SectionDivider />
+    <br />
+    <SectionTitle>Accomplishments</SectionTitle>
+    <Boxes>
+      {accomplishments.map((card, index) => (
+        <Box key={index}>
+          <BoxNum>{card.rank}</BoxNum>
+          <BoxText>{card.text}</BoxText>
+        </Box>
+      ))}
+    </Boxes>
+  </Section>
+);
+
+export default Acomplishments;
