2da1770c3122c389110fe817af1a6555f7e8ed92
safinghoghabori <safinghoghabori65@gmail.com>
2022-01-24 20:22:56 +0530

    about me section updated

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -13,14 +13,14 @@ const AboutMe = () => {
       <SectionTitle>About Me</SectionTitle>
       <SectionText>
         I’m a Full-Stack Developer. I have a passion for writing clean and
-        modular code. I am also keen of maintaining beautiful and responsive UI.
+        modular code. I am also keen on maintaining beautiful and responsive UI.
         <br />
         <br />
-        My favourite stack: React, Node, Express, Mongodb
+        My favourite stack: React, Node, Express, MongoDB
         <br />
         <br />
-        Nowadays i'm exploring more about Javascript and its different
-        frameowrks / libraries.
+        Nowadays I'm exploring more about Javascript and its different
+        frameworks/libraries.
       </SectionText>
     </Section>
   );
